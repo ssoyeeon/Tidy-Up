7fc40b77145c022c3f5690549e54381662a8a7e6
ssoyeeon <ssuu2039@gmail.com>
2024-10-22 22:39:44 +0900

    [fix] 아 왜 오류남?

--- Tidy-Up/Assets/Scripts/ObjectManager/ObjectControlManager.cs
+++ Tidy-Up/Assets/Scripts/ObjectManager/ObjectControlManager.cs
@@ -7,7 +7,6 @@ public class ObjectControlManager : MonoBehaviour
     //리스트에 넣고, 오브젝트 컨트롤에서 다 되면 리스트에서 빼야햄
     public List<ObjectControl> objectsList = new List<ObjectControl>();
     public bool isDone;
-    ObjectControl objectControl;
 
     void Start()
     {
@@ -16,10 +15,6 @@ public class ObjectControlManager : MonoBehaviour
 
     void Update()
     {
-        if (objectControl.isDone == true)
-        {
-            //objectsList.Remove(ObjectControl);        //왜 objectControl 두 개로 잡힐까여??ㅠㅠㅠ 어떻게 해야하는지... 모르겠어요..
-        }
         AllCheckDone();
     }
 
